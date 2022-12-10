318bb1024341587b35a278eb0566b7af71d553ab
Ewen Tsai <ewen.chua.gi@gmail.com>
2022-12-10 20:02:48 +0800

    refactor

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import pandas as pd
 from model import ch_calculator
 
 if __name__ == '__main__':
-    with open("./data.json", 'r') as data:
+    with open("CH_data.json", 'r') as data:
         data_dict = json.load(data)
 
     input_data_frame = pd.read_csv('shipping.csv', skiprows=0)
     for index, data in input_data_frame.iterrows():
-        input_data_frame.loc[index, 'CH'] = ch_calculator.calculate(data_dict['CH'], data.to_dict())
+        input_data_frame.loc[index, 'CH'] = ch_calculator.calculate(data_dict, data.to_dict())
 
     input_data_frame.to_csv('shipping.csv')
