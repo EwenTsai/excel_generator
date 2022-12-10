695e333bffa5ba34c561c19c15e03d2d190d45e8
Ewen Tsai <ewen.chua.gi@gmail.com>
2022-12-10 23:38:39 +0800

    refactor

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ from model import Calculator
 
 if __name__ == '__main__':
     with open("CH_data.json", 'r') as data:
-        data_dict = json.load(data)
+        CH_data_dict = json.load(data)
 
-    input_data_frame = pd.read_csv('shipping.csv', skiprows=0)
+    with open("4PX_data.json", 'r') as data:
+        _4PX_data_dict = json.load(data)
+
+    input_data_frame = pd.read_csv('shipping.csv')
     for index, data in input_data_frame.iterrows():
-        input_data_frame.loc[index, 'CH'] = Calculator.calculate_1(data_dict, data.to_dict())
+        input_data_frame.loc[index, 'CH'] = Calculator.calculate(CH_data_dict, data.to_dict())
+        input_data_frame.loc[index, '4PX'] = Calculator.calculate(_4PX_data_dict, data.to_dict())
 
-    input_data_frame.to_csv('shipping.csv')
+    input_data_frame.to_csv('shipping.csv', index=False)
--- model/Calculator.py
+++ model/Calculator.py
@@ -4,26 +4,46 @@ def match_weight(shipping_list, weight):
             return shipping
 
 
-def match_country(channel_list, country):
-    for channel_country in channel_list:
-        if channel_country == country:
-            return channel_country
+def match_country(data_dict, country):
+    for channel in data_dict:
+        if data_dict[channel].get(country) is not None:
+            return data_dict[channel].get(country)
+
+
+def weight_rounding(weight, ladder):
+    if weight < ladder:
+        return ladder
+    elif weight % ladder == 0:
+        return weight
+    else:
+        return (weight // ladder + 1) * ladder
+
+
+def calculate(data_dict, input_data):
+    shipping_list = match_country(data_dict, input_data['country'])
+    shipping = match_weight(shipping_list, input_data['weight'])
+    if shipping.get('shipping_cost') is not None and shipping.get('extra_charge') is not None:
+        return calculate_1(shipping, input_data)
+    elif shipping.get('first_weight') is not None and shipping.get('continue') is not None and shipping.get(
+            'ladder') is not None:
+        return calculate_2(shipping, input_data)
 
 
 # 根据重量的不同，运费加额外费用算法
-def calculate_1(data_dict, input_data):
-    if input_data['channel'] is not None:
-        shipping_list = data_dict[input_data['channel']][input_data['country']]
-        shipping = match_weight(shipping_list, input_data['weight'])
-        return input_data['weight'] * shipping['shipping_cost'] + shipping['extra_charge']
+def calculate_1(shipping, input_data):
+    return input_data['weight'] * shipping['shipping_cost'] + shipping['extra_charge']
 
 
 # 首重加续重算法
-def calculate_2(data_dict, input_data):
-    shipping_list = data_dict[input_data['channel']][input_data['country']]
-    shipping = match_weight(shipping_list, input_data['weight'])
+def calculate_2(shipping, input_data):
+    weight = weight_rounding(input_data['weight'], shipping['ladder'])
+    if weight == shipping['ladder']:
+        return shipping['first_weight']
+    else:
+        return shipping['first_weight'] + shipping['continue'] * (weight / shipping['ladder'] - 1)
 
 
 # 重量对应运费算法
-def calculate_3(data_dict, input_data):
-    return 0
+def calculate_3(shipping, input_data):
+    weight = weight_rounding(input_data['weight'], shipping['ladder'])
+    print(weight)
