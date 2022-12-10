bdd1946977a6dd597c5f855c553622409c91618b
Ewen Tsai <ewen.chua.gi@gmail.com>
2022-12-10 19:50:01 +0800

    refactor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 
 from model import ch_calculator
 
@@ -6,18 +7,8 @@ if __name__ == '__main__':
     with open("./data.json", 'r') as data:
         data_dict = json.load(data)
 
-    with open("./input.json", 'r') as input_data:
-        input_list = json.load(input_data)
+    input_data_frame = pd.read_csv('shipping.csv', skiprows=0)
+    for index, data in input_data_frame.iterrows():
+        input_data_frame.loc[index, 'CH'] = ch_calculator.calculate(data_dict['CH'], data.to_dict())
 
-    with open("./dict.json", 'r') as name_data:
-        name_dict = json.load(name_data)
-
-    # CH
-    f = open('result.csv', 'w')
-    for input_data in input_list:
-        input_data['CH'] = ch_calculator.calculate(data_dict['CH'], input_data)
-        f.write(input_data['country'] + ',')
-        f.write(input_data['channel'] + ',')
-        f.write(str(input_data['weight']) + ',')
-        f.write(str(input_data['CH']) + ','+'\n')
-    f.close()
+    input_data_frame.to_csv('shipping.csv')
